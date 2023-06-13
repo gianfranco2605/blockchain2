aa6f435f0cd238193762b7291ce01524247d3caf
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-06-13 11:24:44 +0200

    hardhat test successfully

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,6 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomicfoundation/hardhat-toolbox");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.20',
-  networks: {
-    sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/hBSYAzcXKDTTy8oc4s4m24jFKAp2mZAK',
-      accounts: ['5f84d016e31ddf2bbd3930fb785e0f5d4107b824c24ab064c33438897c3a0c6a'],
-    },
-  },
+  solidity: "0.8.18",
 };
